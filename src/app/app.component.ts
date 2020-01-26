3b54ef1129acaa0d2a1adfeb0c4bab727f46803e
fsajadi <s.sfatemeh@gmail.com>
2020-01-25 20:10:04 -0500

    star icon can be either selected or unselected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'movieRating';
 
+  movies = [
+    {
+      image: "1.jpg",
+      isLiked: true,
+    },
+    {
+      image: "2.jpg",
+      isLiked: true,
+    },
+    {
+      image: "3.jpg",
+      isLiked: false,
+    },
+    {
+      image: "4.jpg",
+      isLiked: true,
+    },
+    {
+      image: "5.jpg",
+      isLiked: true,
+    }
+  ]
 
 
-  images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]
+  clickEvent(i) {
+    this.movies[i].isLiked = !this.movies[i].isLiked;
 
-  condition: boolean = true;
-  isLiked: boolean = true;
-  clickEvent($event) {
-    this.condition = !this.condition;
-    console.log($event.target.value)
+
   }
 }
