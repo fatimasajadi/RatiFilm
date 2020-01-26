3137301811463e6fdf5bb1f4a7493b9ec91e5c64
fsajadi <s.sfatemeh@gmail.com>
2020-01-25 21:37:00 -0500

    thumbs-up icon and like feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,56 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'movieRating';
+  numberOfLikes;
+  isLiked: boolean = true;
+
+  likesCount(j) {
+    if (this.movies[j].isLiked) {
+      this.movies[j].numberOfLikes = (this.movies[j].numberOfLikes) - 1,
+        this.movies[j].isLiked = !this.movies[j].isLiked
+
+    } else {
+      this.movies[j].numberOfLikes = (this.movies[j].numberOfLikes) + 1,
+        this.movies[j].isLiked = !this.movies[j].isLiked
+    }
+  }
 
   movies = [
     {
       image: "1.jpg",
+      isFavorite: true,
       isLiked: true,
+      numberOfLikes: 1
     },
     {
       image: "2.jpg",
-      isLiked: true,
+      isFavorite: true,
+      isLiked: false,
+      numberOfLikes: 2
     },
     {
       image: "3.jpg",
-      isLiked: false,
+      isFavorite: false,
+      isLiked: true,
+      numberOfLikes: 3
     },
     {
       image: "4.jpg",
+      isFavorite: true,
       isLiked: true,
+      numberOfLikes: 4
     },
     {
       image: "5.jpg",
+      isFavorite: true,
       isLiked: true,
+      numberOfLikes: 5
     }
   ]
 
 
   clickEvent(i) {
-    this.movies[i].isLiked = !this.movies[i].isLiked;
+    this.movies[i].isFavorite = !this.movies[i].isFavorite;
 
 
   }
