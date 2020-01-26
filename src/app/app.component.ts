3a2b45d6a6b9130bdc6e92fb4ce0a157346bd813
fsajadi <s.sfatemeh@gmail.com>
2020-01-26 00:24:43 -0500

    fetch movies from json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,12 @@ export class AppComponent {
   title = 'movieRating';
   numberOfLikes;
   isLiked: boolean = true;
+  film = ''
+
+
+  constructor() {
+    this.movieFetch();
+  }
 
   likesCount(j) {
     if (this.movies[j].isLiked) {
@@ -22,37 +28,45 @@ export class AppComponent {
     }
   }
 
+  movieFetch() {
+    fetch("assets/data.json").then(response => response.json()).then(obj => {
+      this.movies = obj;
+    })
+
+
+  }
+
   movies = [
-    {
-      image: "1.jpg",
-      isFavorite: true,
-      isLiked: true,
-      numberOfLikes: 1
-    },
-    {
-      image: "2.jpg",
-      isFavorite: true,
-      isLiked: false,
-      numberOfLikes: 2
-    },
-    {
-      image: "3.jpg",
-      isFavorite: false,
-      isLiked: true,
-      numberOfLikes: 3
-    },
-    {
-      image: "4.jpg",
-      isFavorite: true,
-      isLiked: true,
-      numberOfLikes: 4
-    },
-    {
-      image: "5.jpg",
-      isFavorite: true,
-      isLiked: true,
-      numberOfLikes: 5
-    }
+    // {
+    //   image: "1.jpg",
+    //   isFavorite: true,
+    //   isLiked: true,
+    //   numberOfLikes: 1
+    // },
+    // {
+    //   image: "2.jpg",
+    //   isFavorite: true,
+    //   isLiked: false,
+    //   numberOfLikes: 2
+    // },
+    // {
+    //   image: "3.jpg",
+    //   isFavorite: false,
+    //   isLiked: true,
+    //   numberOfLikes: 3
+    // },
+    // {
+    //   image: "4.jpg",
+    //   isFavorite: true,
+    //   isLiked: true,
+    //   numberOfLikes: 4
+    // },
+    // {
+    //   image: "5.jpg",
+    //   isFavorite: true,
+    //   isLiked: true,
+    //   numberOfLikes: 5
+    // }
   ]
 
 
