35e99ecac1e0c4ec613003d41a5643da03796539
fsajadi <s.sfatemeh@gmail.com>
2020-01-25 19:29:13 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'movieRating';
+
+
+
+  images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]
+
+  condition: boolean = true;
+  isLiked: boolean = true;
+  clickEvent($event) {
+    this.condition = !this.condition;
+    console.log($event.target.value)
+  }
 }
